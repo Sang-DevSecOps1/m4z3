2b99b49d18a5eceb0ce0c415e00f50aeea27c24b
Maze ❤️‍🔥️ <daviddorcassangbot@gmail.com>
2024-05-16 19:16:04 +0100

    Re-Home page, signup page, and login page styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,52 +1,37 @@
-const hero = document.querySelector(".hero");
+// const hero = document.querySelector(".hero");
 const colorButton = document.querySelector(".color-button");
 const background = [
   {
-    color: "#011627", // Purple color
-    writeup1: "Without secure APIs, rapid innovation would be impossible",
-    writeup2: "Get your APIs Scanned to avoid business downtime",
+    color: "#160b37",
+    // writeup1: "Without secure APIs, rapid innovation would be impossible",
+    // writeup2: "Get your APIs Scanned to avoid business downtime",
   },
   {
-    color: "rgb(0, 0, 43)", // Darker blue
-    writeup1: "Without secure APIs, rapid innovation would be impossible",
-    writeup2: "Get your APIs Scanned to avoid business downtime",
-  },
-  {
-    color: "rgb(96, 34, 12)", // Dark Orange
-    writeup1: "Without secure APIs, rapid innovation would be impossible",
-    writeup2: "Get your APIs Scanned to avoid business downtime",
-  },
-  {
-    color: "#011627", // Darker gray
-    writeup1: "Without secure APIs, rapid innovation would be impossible",
-    writeup2: "Get your APIs Scanned to avoid business downtime",
-  },
-  {
-    color: "rgb(27, 1, 27)", // Dark Purple
-    writeup1: "Without secure APIs, rapid innovation would be impossible",
-    writeup2: "Get your APIs Scanned to avoid business downtime",
+    color: "rgb(0, 0, 43)",
+    // writeup1: "Without secure APIs, rapid innovation would be impossible",
+    // writeup2: "Get your APIs Scanned to avoid business downtime",
   },
 ];
 
 let currentIndex = 0;
 
-function changeBackgrounColor() {
-  const currentColor = background[currentIndex];
-  hero.style.background = currentColor.color;
-  const writeupElement1 = document.querySelector(".hero-heading");
-  writeupElement1.innerHTML = currentColor.writeup1;
-  const writeupElement2 = document.querySelector(".hero-paragraph");
-  writeupElement2.innerHTML = currentColor.writeup2;
-  currentIndex = (currentIndex + 1) % background.length;
-}
+// function changeBackgrounColor() {
+//   const currentColor = background[currentIndex];
+//   hero.style.background = currentColor.color;
+//   const writeupElement1 = document.querySelector(".hero-heading");
+//   writeupElement1.innerHTML = currentColor.writeup1;
+//   const writeupElement2 = document.querySelector(".hero-paragraph");
+//   writeupElement2.innerHTML = currentColor.writeup2;
+//   currentIndex = (currentIndex + 1) % background.length;
+// }
 function changeButtonColor() {
   const currentColor = background[currentIndex];
   colorButton.style.background = currentColor.color;
   currentIndex = (currentIndex + 1) % background.length;
 }
 
-changeBackgrounColor();
+// changeBackgrounColor();
 changeButtonColor;
 
-setInterval(changeBackgrounColor, 8000);
+// setInterval(changeBackgrounColor, 2000);
 setInterval(changeButtonColor, 7000);
